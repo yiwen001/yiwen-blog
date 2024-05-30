fc3779bc43e400083c8c4711b6c352efec275969
yiwen321 <ywgao1998@163.com>
2024-05-30 17:28:22 +0800

    feat: 字体更改

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import styles from "./page.module.css";
-
+import {Button} from "@nextui-org/button";
 export default function Home() {
   return (
     <div className={styles.main}>
-         Yiwen 
-        <span>say hi👋</span>
+      <div className={styles.nav}></div>
+       <div className={styles.content}>  
+       <div className={styles.mainline}>  Hi, I am Yiwen  </div>
+     
+       <Button className={styles.customButton}>
+       About me 👋
+      </Button> 
+        
+        </div> 
+        <div className={styles.footer}>
+        <div className={styles.icon} id={styles.github}></div>
+      <div className={styles.icon} id={styles.linkedin}></div>
+      <div className={styles.icon} id={styles.email}></div>
+      <div className={styles.icon} id={styles.medium}></div>
+      <div className={styles.icon} id={styles.instagram}></div>
+ 
+
+        </div>
+       
        
     </div>
   );
