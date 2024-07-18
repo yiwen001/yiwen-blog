093ada02f546ae8661c3cb87f9100ad8c63e2f63
yiwen321 <ywgao1998@163.com>
2024-07-18 17:30:47 +0800

    feat: 导航router

--- src/app/content/about/page.tsx
+++ src/app/content/about/page.tsx
@@ -1,10 +1,59 @@
-
-import React from 'react';
-import styles from './page.module.css';
+"use client";
+import React, { useState } from 'react';
+import styles from './page.module.sass';
+import { Dropdown, Menu } from 'antd';
+import { useRouter } from 'next/navigation';
 export default function AboutPage() {
+  const [visible, setVisible] = useState(false);
+  const router = useRouter();
+  const handleVisibleChange = (flag) => {
+    setVisible(flag);
+  };
+ 
+  const handleMenuClick = (e) => {
+    if (e.key === 'home') {
+      router.push('/');
+    }
+  };
+  const menu = (
+    <Menu   
+    onClick={handleMenuClick}
+     className={styles.customMenu}>
+      <Menu.Item key="home" className={styles.menuItem}>Home</Menu.Item>
+      <Menu.Item key="resume" className={styles.menuItem}>Resume</Menu.Item>
+      <Menu.Item key="blog" className={styles.menuItem}>Blog</Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div className={styles.main}>
-      <h1>About Me</h1>
+    <div className={styles.aboutSection}>
+      <div className={styles.secondMain}>
+        <div className={styles.secondNav}>
+          <div>About Me</div>
+          <Dropdown 
+            overlay={menu} 
+            trigger={['click']} 
+            visible={visible} 
+            onVisibleChange={handleVisibleChange}>
+            <div 
+              className={styles.icon} 
+              id={styles.menu} 
+              onClick={() => setVisible(!visible)}>
+            </div>
+          </Dropdown>
+        </div>
+        <div className={styles.intro}>
+          <div>
+            Hi! I'm XXX, a Front-end Web Developer based in Shaoxing, China. Having recently completed my Master's in Computer Engineering from NYU in 2023, I now apply my skills at a distinguished university's institute. Here, I'm actively engaged in a significant project—a Virtual Power Plant (VPP) platform, which serves as a cutting-edge solution for smart energy resource management. My responsibilities span the entire development lifecycle, from design to deployment, bolstered by the automation of our deployment process for optimized testing efficiency and reliability.
+          </div>
+          <div className={styles.intro2}>
+            <div style={{ width: '700px' }}>
+              My technical arsenal includes JavaScript/TypeScript, React, Umi, Node.js, Next.js, and Nginx. I'm well-versed in utilizing data visualization tools and UI libraries like ECharts, Ant Design (antd), and Chakra UI, with proven experience in managing large datasets on the front-end.
+            </div>
+            <div className={styles.avatar}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client"
-import styles from "./page.module.css";
+import styles from "./page.module.sass";
 import {Button} from "@nextui-org/button";
-import { useRouter } from 'next/navigation';
-import React, { useRef,useState } from 'react';
+import { useRouter  } from 'next/navigation';
+import React, { useRef,useState,useEffect} from 'react';
 export default function Home() {
   const router = useRouter();
-  const aboutRef = useRef<HTMLDivElement>(null);
-  const [isVisible, setIsVisible] = useState(false);
   const handleClick = () => {
-    setIsVisible(true); // 设置 About Me 部分为可见
-    setTimeout(() => {
-      aboutRef.current?.scrollIntoView({ behavior: 'smooth' });
-    },100); // 稍微延迟滚动以确保 CSS 过渡生效
+   router.push('/content/about');
   };
+ 
+ 
   return (
     <>
     <div className={styles.main}>
@@ -20,12 +17,11 @@ export default function Home() {
         <div className={styles.icon} id={styles.menu} style={{width:'25px',height:'25px'}}></div>
       </div>
        <div className={styles.content}>  
-       <div className={styles.mainline}>  Hi, I am Yiwen  </div>
+       <div className={styles.mainline}>  Hi, I am XXXXX  </div>
      
        <Button className={styles.customButton}  onClick={handleClick}>
        About me 👋
       </Button> 
-        
         </div> 
         <div className={styles.footer}>
         <a href="https://github.com/yiwen001" target="_blank">
@@ -41,39 +37,10 @@ export default function Home() {
       <a href="https://www.instagram.com/1vvenn/" target="_blank">
     
       <div className={styles.icon} id={styles.instagram}></div></a>
- 
-
         </div>
-
-     
-   
-       
+  
     </div>
-    <div ref={aboutRef} className={`${styles.aboutSection} ${isVisible ? styles.aboutSectionVisible : ''}`}>
-      <div className={styles.secondMain}>
-      <div className={styles.secondNav}>
-     <div> About Me</div> 
-     <div className={styles.icon} id={styles.menu} style={{width:'20px',height:'20px'}}></div></div>
-     <div className={styles.intro}>
-      <p>
-      Hi, I am Rony! I'm a designer with 3 years of web development
-      experience with the past 2 years spent honing my skills in
-      WordPress, Shopify. And Webflow
-      Hi, I am Rony! I'm a designer with 3 years of web development
-      experience with the past 2 years spent honing my skills in
-      WordPress, Shopify. And Webflow
-      </p>
-      <div className={styles.intro2}>
-     
-      Hi, I am Rony! I'm a designer with 3 years of web development
-      experience with the past 2 years spent honing my skills in
-      WordPress, Shopify. And Webflow
-    
-      <div className={styles.avatar}></div>
-      </div>
-     </div>
-     </div>
- </div>
+
  
 
  </>
